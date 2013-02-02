7b87b1243a7f34a606bb54eb011332605451e6e7
KenR <dev@kenpire.com>
2013-02-02 13:52:55 +0000

    WIP Login via RefUnited platform

---
+++ RefUnitedIVRPlatform.Data/Managers/RefugeesUnitedAccountManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RefugeesUnitedApi;
+using RefUnitedIVRPlatform.Common.Interfaces;
+
+namespace RefUnitedIVRPlatform.Data.Managers
+{
+  public class RefugeesUnitedAccountManager : IRefugeesUnitedAccountManager
+  {
+    private ApiRequestSettings apiRequestSettings = new ApiRequestSettings()
+    {
+    };
+
+    public bool ValidateLogin(string username, string password)
+    {
+      ApiRequest request = new ApiRequest(apiRequestSettings);
+
+      var loginRequest = request.ProfileLogin(username, password);
+
+      if (loginRequest == null)
+      {
+        return false;
+      }
+
+      return loginRequest.Authenticated;
+    }
+  }
+}
