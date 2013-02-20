ef104d6d03702a95fafa95d9aaaf649aafbee7ec
KenR <dev@kenpire.com>
2013-02-20 19:19:21 +0000

    Added Business test project

---
+++ RefUnitedIVRPlatform.Business.Tests/RefugeesUnitedAccountManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefUnitedIVRPlatform.Business.Tests
+{
+  [TestClass]
+  public class RefugeesUnitedAccountManagerTests
+  {
+    [TestClass]
+    public class GetFavouritesTests
+    {
+      [TestMethod]
+      public void ShouldReturnAListOfFavourites()
+      {
+        Assert.Inconclusive();
+      }
+    }
+  }
+}
