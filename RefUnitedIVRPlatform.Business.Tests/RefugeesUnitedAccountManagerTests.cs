320b0d35770277016f9c2ece78d3eaab7a2bc857
KenR <dev@kenpire.com>
2013-02-21 21:18:33 +0000

    Added passing tests

--- RefUnitedIVRPlatform.Business.Tests/RefugeesUnitedAccountManagerTests.cs
+++ RefUnitedIVRPlatform.Business.Tests/RefugeesUnitedAccountManagerTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefUnitedIVRPlatform.Business.Managers;
+using RefugeesUnitedApi;
+using Moq;
+using System.Collections.Generic;
+using RefUnitedIVRPlatform.Common.Entities;
 
 namespace RefUnitedIVRPlatform.Business.Tests
 {
@@ -9,10 +14,49 @@ namespace RefUnitedIVRPlatform.Business.Tests
     [TestClass]
     public class GetFavouritesTests
     {
-      [TestMethod]
-      public void ShouldReturnAListOfFavourites()
+      List<RefugeesUnitedApi.ApiEntities.Profile> favourites;
+
+      [TestInitialize]
+      public void Setup()
       {
-        Assert.Inconclusive();
+         favourites = new List<RefugeesUnitedApi.ApiEntities.Profile>();
+
+         for (int i = 0; i < 14; i++)
+         {
+           favourites.Add(new RefugeesUnitedApi.ApiEntities.Profile());
+         }
+      }
+
+      [TestMethod]
+      public void ShouldReturnNineFavouritesFirstPage()
+      {
+        int profileId = 324784;
+
+        var apiRequest = new Mock<IApiRequest>();
+        apiRequest.Setup(m => m.GetFavourites(profileId)).Returns(favourites);
+
+        var manager = new RefugeesUnitedAccountManager(apiRequest.Object);
+
+        var favs = manager.GetFavourites(profileId, 0);
+
+        Assert.IsNotNull(favs);
+        Assert.AreEqual(9, favs.Count);
+      }
+
+      [TestMethod]
+      public void ShouldReturnFiveFavouritesFromSecondPage()
+      {
+        int profileId = 324784;
+
+        var apiRequest = new Mock<IApiRequest>();
+        apiRequest.Setup(m => m.GetFavourites(profileId)).Returns(favourites);
+
+        var manager = new RefugeesUnitedAccountManager(apiRequest.Object);
+
+        var favs = manager.GetFavourites(profileId, 1);
+
+        Assert.IsNotNull(favs);
+        Assert.AreEqual(5, favs.Count);
       }
     }
   }
