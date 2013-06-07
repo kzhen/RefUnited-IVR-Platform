35dac018c55097cfe11e03b893d71b60677e3096
KenR <dev@kenpire.com>
2013-06-07 08:36:45 +0100

    Added some business logic tests

---
+++ RefUnitedIVRPlatform.Business.Tests/LanguageHelperTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RefUnitedIVRPlatform.Business.Tests
+{
+  [TestClass]
+  public class LanguageHelperTests
+  {
+    [TestClass]
+    public class GetValidCultureTests
+    {
+      [TestMethod]
+      public void ShouldReturnDefaultCulture()
+      {
+        var expectedResult = LanguageHelper.GetDefaultCulture();
+
+        var culture = LanguageHelper.GetValidCulture(null);
+
+        Assert.AreEqual(expectedResult, culture);
+      }
+
+      [TestMethod]
+      public void ShouldReturnAKnownCulture()
+      {
+        var expected = "en";
+
+        var culture = LanguageHelper.GetValidCulture("en");
+
+        Assert.AreEqual(expected, culture);
+      }
+
+      [TestMethod]
+      public void ShouldReturnAClosestMatch()
+      {
+        var expected = "en";
+
+        var culture = LanguageHelper.GetValidCulture("en-US");
+
+        Assert.AreEqual(expected, culture);
+      }
+
+      [TestMethod]
+      public void ShouldReturnDefaultCultureIfNoMatch()
+      {
+        var expected = "en";
+
+        var culture = LanguageHelper.GetValidCulture("fr");
+
+        Assert.AreEqual(expected, culture);
+      }
+    }
+
+    [TestClass]
+    public class IsImplementedAsMP3Tests
+    {
+      [TestMethod]
+      public void ShouldReturnTrueIfItIsImplementedAsMP3()
+      {
+        var expected = true;
+
+        var isImplementedAsMP3 = LanguageHelper.IsImplementedAsMP3("ar");
+
+        Assert.AreEqual(expected, isImplementedAsMP3);
+      }
+
+      [TestMethod]
+      public void ShouldReturnFalseIfItIsntImplementedAsMP3()
+      {
+        var expected = false;
+
+        var isImplementedAsMP3 = LanguageHelper.IsImplementedAsMP3("en");
+
+        Assert.AreEqual(expected, isImplementedAsMP3);
+      }
+    }
+  }
+}
---
+++ RefUnitedIVRPlatform.Business.Tests/TwiMLHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio.TwiML;
+
+namespace RefUnitedIVRPlatform.Business.Tests
+{
+  [TestClass]
+  public class TwiMLHelperTests
+  {
+    [TestClass]
+    public class SayOrPlayTests
+    {
+      [TestMethod]
+      public void ShouldUsePlayVerbIfImplementedAsMP3()
+      {
+        TwilioResponse mockResponse = new TwilioResponse();
+
+        TwiMLHelper helper = new TwiMLHelper("ar", true);
+
+        helper.SayOrPlay(mockResponse, "test message");
+
+        Assert.IsTrue(mockResponse.Element.ToString().Contains("<Play>"));
+      }
+
+      [TestMethod]
+      public void ShouldUseSayVerbIfNotImplementedAsMP3()
+      {
+        TwilioResponse mockResponse = new TwilioResponse();
+
+        TwiMLHelper helper = new TwiMLHelper("en", false);
+
+        helper.SayOrPlay(mockResponse, "test message");
+
+        Assert.IsTrue(mockResponse.Element.ToString().Contains("<Say language=\"en\">"));
+      }
+    }
+  }
+}
