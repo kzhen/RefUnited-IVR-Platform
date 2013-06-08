13e40bc5fda3fa390ea76e76a1da5fdd72772f3d
KenR <dev@kenpire.com>
2013-06-08 12:43:06 +0100

    Added tests for Delete and LookupByPhoneNumber

--- RefUnitedIVRPlatform.Data.Tests/ProfileRepositoryAzureIntegrationTests.cs
+++ RefUnitedIVRPlatform.Data.Tests/ProfileRepositoryAzureIntegrationTests.cs
@@ -81,5 +81,94 @@ namespace RefUnitedIVRPlatform.Data.Tests
         Assert.IsNotNull(user);
       }
     }
+
+    [TestClass]
+    public class DeleteTests
+    {
+      [TestInitialize]
+      public void Setup()
+      {
+        var storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+        var tableClient = storageAccount.CreateCloudTableClient();
+        var table1 = tableClient.GetTableReference("ivrProfile");
+        table1.DeleteIfExists();
+        table1.Create();
+
+        var entity = new IVRProfileEntity()
+        {
+          ProfileId = 123456,
+          FullName = "Test User1",
+          PhoneNumber = "+1111111",
+          RowKey = "123456",
+          PartitionKey = (123456).ToString().Substring(0, 2)
+        };
+
+        var op = TableOperation.Insert(entity);
+        table1.Execute(op);
+      }
+
+      [TestMethod]
+      public void ShouldDeleteUserProfile()
+      {
+        int profileId = 123456;
+
+        ProfileRepositoryAzure repository = new ProfileRepositoryAzure(DEVELOPMENT_CONNECTION_STRING);
+
+        var result = repository.Delete(new IVRProfile() { ProfileId = profileId });
+
+        Assert.AreEqual(true, result);
+      }
+
+      [TestMethod]
+      public void ShouldReturnFalseWhenUserProfileCannotBeDeleted()
+      {
+        int profileId = 5555555;
+
+        ProfileRepositoryAzure repository = new ProfileRepositoryAzure(DEVELOPMENT_CONNECTION_STRING);
+
+        var result = repository.Delete(new IVRProfile() { ProfileId = profileId });
+
+        Assert.AreEqual(false, result);
+      }
+    }
+
+    [TestClass]
+    public class GetByPhoneNumberTests
+    {
+      [TestInitialize]
+      public void Setup()
+      {
+        var storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+        var tableClient = storageAccount.CreateCloudTableClient();
+        var table1 = tableClient.GetTableReference("ivrProfile");
+        table1.DeleteIfExists();
+        table1.Create();
+
+        var entity = new IVRProfileEntity()
+        {
+          ProfileId = 123456,
+          FullName = "Test User1",
+          PhoneNumber = "+1111111",
+          RowKey = "123456",
+          PartitionKey = (123456).ToString().Substring(0, 2)
+        };
+
+        var op = TableOperation.Insert(entity);
+        table1.Execute(op);
+      }
+
+      [TestMethod]
+      public void ShouldReturnAUserProfileByLookingUpPhoneNumber()
+      {
+        var phoneNumberToLookup = "+1111111";
+
+        ProfileRepositoryAzure repository = new ProfileRepositoryAzure(DEVELOPMENT_CONNECTION_STRING);
+
+        var userPorfile = repository.GetByPhoneNumber(phoneNumberToLookup);
+
+        Assert.IsNotNull(userPorfile);
+        Assert.AreEqual(phoneNumberToLookup, userPorfile.PhoneNumber);
+      }
+    }
   }
 }
