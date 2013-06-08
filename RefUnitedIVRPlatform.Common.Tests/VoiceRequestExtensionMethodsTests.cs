c475e7c0a0d2c03084a1a862f1e1fcf0a7adfbed
KenR <dev@kenpire.com>
2013-06-08 16:54:32 +0100

    Added VoiceRequestExtensionMethod tests

---
+++ RefUnitedIVRPlatform.Common.Tests/VoiceRequestExtensionMethodsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twilio.Mvc;
+
+namespace RefUnitedIVRPlatform.Common.Tests
+{
+  [TestClass]
+  public class VoiceRequestExtensionMethodsTests
+  {
+    [TestClass]
+    public class GetOriginatingNumberTests
+    {
+      [TestMethod]
+      public void ShouldReturnOriginatingNumberForInboundCall()
+      {
+        VoiceRequest request = new VoiceRequest()
+        {
+          Direction = "inbound",
+          From = "+1234567890"
+        };
+
+        var originatingNumber = request.GetOriginatingNumber();
+
+        Assert.AreEqual("+1234567890", originatingNumber);
+      }
+
+      [TestMethod]
+      public void ShouldReturnOriginatingNumberForOutboundCall()
+      {
+        VoiceRequest request = new VoiceRequest()
+        {
+          Direction = "outbound-api",
+          To = "+1234567890"
+        };
+
+        var originatingNumber = request.GetOriginatingNumber();
+
+        Assert.AreEqual("+1234567890", originatingNumber);
+      }
+    }
+  }
+}
