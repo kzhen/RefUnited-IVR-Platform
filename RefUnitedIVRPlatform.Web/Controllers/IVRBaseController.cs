2994c1895c4f6b8836fb125cf2a10bb3b79014c7
KenR <dev@kenpire.com>
2013-06-15 13:11:36 +0300

    Added new IVRBaseController

---
+++ RefUnitedIVRPlatform.Web/Controllers/IVRBaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.TwiML;
+
+namespace RefUnitedIVRPlatform.Web.Controllers
+{
+  public class IVRBaseController : Controller
+  {
+    public ActionResult IVRResult(TwilioResponse response)
+    {
+      Response.ContentType = "text/xml";
+      return Content(response.Element.ToString());
+    }
+  }
+}
